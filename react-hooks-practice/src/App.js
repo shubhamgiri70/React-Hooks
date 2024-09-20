bd565cf346432584582708dd70d81003abe66beb
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-09-20 21:06:09 +0530

    learned more about hooks

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 import "../src/App.css";
 
 function App() {
   let [value, setValue] = useState(0);
+
+  useEffect(() => {
+    document.title = `Counter: ${value}`;
+    console.log("from useEffect");
+  }, [value]);
 
   return (
     <div className="app">
